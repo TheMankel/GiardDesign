47cad9a285f0e9a302b38580aa62f37f4b1e4db9
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-08-18 16:42:23 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,105 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#111111',
+        'green-pea': {
+          DEFAULT: '#1B5B31',
+          50: '#5ECF85',
+          100: '#4FCB79',
+          200: '#37B964',
+          300: '#2E9A53',
+          400: '#247A42',
+          500: '#1B5B31',
+          600: '#0E301A',
+          700: '#010402',
+          800: '#000000',
+          900: '#000000',
+          950: '#000000',
+        },
+        cameo: {
+          DEFAULT: '#DCC1AB',
+          50: '#FAF6F3',
+          100: '#F4ECE5',
+          200: '#E8D6C8',
+          300: '#DCC1AB',
+          400: '#CCA483',
+          500: '#BB875C',
+          600: '#9D6B41',
+          700: '#765031',
+          800: '#4E3520',
+          900: '#261A10',
+          950: '#130D08',
+        },
+        'spring-wood': {
+          DEFAULT: '#F5F0EC',
+          50: '#FCFBF9',
+          100: '#F5F0EC',
+          200: '#E2D3C7',
+          300: '#CEB6A2',
+          400: '#BB997E',
+          500: '#A87C59',
+          600: '#836145',
+          700: '#5F4632',
+          800: '#3A2B1E',
+          900: '#15100B',
+          950: '#030201',
+        },
+        'ad-respect': '#003EDC',
+      },
+      fontFamily: {
+        inter: ['inter', 'sans-serif'],
+        montserrat: ['montserrat', 'sans-serif'],
+      },
+    },
+    backgroundImage: {
+      hero: 'url("/assets/images/Hero.webp")',
+    },
+    animation: {
+      'fade-in': 'fadeIn 300ms ease-in-out',
+      'fade-out': 'fadeOut 300ms ease-in-out',
+      'slide-in': 'slideIn 300ms ease-in-out',
+      'slide-out': 'slideOut 300ms ease-in-out',
+    },
+    keyframes: {
+      fadeIn: {
+        '0%': {
+          opacity: 0,
+        },
+        '100%': {
+          opacity: 1,
+        },
+      },
+      fadeOut: {
+        '0%': {
+          opacity: 1,
+        },
+        '100%': {
+          opacity: 0,
+        },
+      },
+      slideIn: {
+        '0%': {
+          opacity: 0,
+          width: 0,
+        },
+        '100%': {
+          opacity: 1,
+          width: '100%',
+        },
+      },
+      slideOut: {
+        '0%': {
+          opacity: 1,
+          width: '100%',
+        },
+        '100%': {
+          opacity: 0,
+          width: 0,
+        },
+      },
+    },
   },
   plugins: [],
 };
