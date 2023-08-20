c0a0646605bf440f91b52d4a156fcae4ad351b89
Jakub Jankowski <jakub.tom.jankowski@gmail.com>
2023-08-20 14:49:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@
 
 const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('menu-overlay');
-const sliderNavigation = document.getElementById('slider-nav');
+const expandBtn = document.getElementById('expand');
+const gallery = document.getElementById('gallery');
+const imagesData = [
+  '/assets/images/Gallery_2.webp',
+  '/assets/images/Gallery_1.webp',
+  '/assets/images/Gallery_3.webp',
+  '/assets/images/Gallery_4.webp',
+  '/assets/images/Gallery_5.webp',
+  '/assets/images/Gallery_6.webp',
+  '/assets/images/Gallery_7.webp',
+  '/assets/images/Gallery_8.webp',
+  '/assets/images/Gallery_9.webp',
+];
 
 function toggleMobileMenu() {
   hamburger.classList.toggle('open');
@@ -12,22 +24,93 @@ function toggleMobileMenu() {
 
 hamburger.addEventListener('click', toggleMobileMenu);
 
-function handleMenuClick(event) {
-  if (event.target.tagName === 'A') {
+function handleMenuClick(e) {
+  if (e.target.tagName === 'A') {
     toggleMobileMenu();
   }
 }
 
 menu.addEventListener('click', handleMenuClick);
 
-function handleSlider(e) {
-  const clickedElement = e.target;
+function toggleGallery() {
+  gallery.classList.toggle('open');
 
-  if (clickedElement.closest('#left-slider-arrow')) {
-    console.log('left');
-  } else if (clickedElement.closest('#right-slider-arrow')) {
-    console.log('right');
+  if (gallery.classList.contains('open')) {
+    imagesData.forEach((imageSrc) => {
+      const imgElement = document.createElement('img');
+      imgElement.className = 'cursor-pointer';
+      imgElement.src = imageSrc;
+      imgElement.alt = 'Hero slider';
+      gallery.appendChild(imgElement);
+    });
+  } else {
+    const imagesToRemove = 9;
+    const images = gallery.querySelectorAll('img');
+    for (
+      let i = images.length - 1;
+      i >= Math.max(images.length - imagesToRemove, 0);
+      i--
+    ) {
+      gallery.removeChild(images[i]);
+    }
   }
 }
 
-sliderNavigation.addEventListener('click', handleSlider);
+function toggleGalleryButton() {
+  const textElement = expandBtn.querySelector('span');
+  const svgIcon = expandBtn.querySelector('svg');
+  expandBtn.classList.toggle('open');
+
+  if (expandBtn.classList.contains('open')) {
+    textElement.textContent = 'Zwiń';
+    svgIcon.classList.add('rotate-180');
+  } else {
+    textElement.textContent = 'Rozwiń';
+    svgIcon.classList.remove('rotate-180');
+  }
+}
+
+function handleExpandClick() {
+  toggleGallery();
+  toggleGalleryButton();
+
+  galleryObj.recalculate(true);
+
+  const images = gallery.querySelectorAll('img');
+  const targetImageIndex = images.length - 4;
+  const targetImage = images[targetImageIndex];
+  console.log(targetImageIndex);
+  console.log(targetImage);
+  targetImage.scrollIntoView({
+    behavior: 'smooth',
+  });
+}
+
+expandBtn.addEventListener('click', handleExpandClick);
+
+new Swiper('.carousel', {
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: '#left-slider-arrow',
+    prevEl: '#right-slider-arrow',
+  },
+});
+
+const galleryObj = new Macy({
+  container: '#gallery',
+  mobileFirst: false,
+  columns: 3,
+  breakAt: {
+    425: 1,
+    768: 2,
+    1024: 3,
+  },
+  margin: {
+    x: 43,
+    y: 43,
+  },
+});
